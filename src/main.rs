c17eb2d7592ec8256b468de789e6d8eddf29bd1e
Talison Fabio <talisonf11@gmail.com>
2025-07-18 19:38:19 -0300

    feat: add feature flag to disable api or worker

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,24 @@ mod models;
 mod repository;
 mod worker;
 
+#[cfg(all(feature = "disable_api", feature = "disable_worker"))]
+compile_error!("You cannot disable both api and worker features");
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let app_context = AppContext::new().await;
 
-    let api_server = tokio::spawn(api::run(app_context.clone(), 3000));
-    let worker = tokio::spawn(worker::run(app_context, 10));
+    let api_server = if cfg!(feature = "disable_api") {
+        tokio::spawn(futures::future::pending())
+    } else {
+        tokio::spawn(api::run(app_context.clone(), 3000))
+    };
+
+    let worker = if cfg!(feature = "disable_worker") {
+        tokio::spawn(futures::future::pending())
+    } else {
+        tokio::spawn(worker::run(app_context, 10))
+    };
 
     tokio::select! {
         v = api_server => Err(anyhow::anyhow!("API server finished: {:?}", v)),
