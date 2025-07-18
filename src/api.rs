434c5dac4faa7ea9b331eb66af8967311ca8ff3e
Talison Fabio <talisonf11@gmail.com>
2025-07-18 18:31:32 -0300

    feat: setup main with api and worker together

--- src/api.rs
+++ src/api.rs
@@ -5,12 +5,18 @@ use crate::context::AppContext;
 
 mod endpoints;
 
-pub async fn app_router() -> Router {
-    let app_context = AppContext::new().await;
-
+pub async fn app_router(ctx: AppContext) -> Router {
     Router::new()
         .route("/ping", get("pong"))
         .route("/nodes", get(endpoints::get_nodes))
         .route("/update", get(endpoints::update_last_nodes))
-        .with_state(app_context)
+        .with_state(ctx)
+}
+
+pub async fn run(ctx: AppContext, port: u16) -> anyhow::Result<()> {
+    let addr = format!("0.0.0.0:{}", port);
+    let api_router = app_router(ctx).await;
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, api_router).await?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use crate::context::AppContext;
+
 mod api;
 mod context;
 mod errors;
 mod handlers;
 mod models;
 mod repository;
+mod worker;
 
 #[tokio::main]
-async fn main() {
-    let app = api::app_router().await;
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+async fn main() -> anyhow::Result<()> {
+    let app_context = AppContext::new().await;
+
+    let api_server = tokio::spawn(api::run(app_context.clone(), 3000));
+    let worker = tokio::spawn(worker::run(app_context, 10));
+
+    tokio::select! {
+        v = api_server => Err(anyhow::anyhow!("API server finished: {:?}", v)),
+        v = worker => Err(anyhow::anyhow!("Worker finished: {:?}", v)),
+    }
 }
